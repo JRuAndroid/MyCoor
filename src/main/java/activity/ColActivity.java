3fcb8c100fc6a8332535627fa07c735473c88d7a
zyskyking <zyskyking@126.com>
2016-07-20 10:12:20 +0800

    组员上传测试

--- src/main/java/activity/ColActivity.java
+++ src/main/java/activity/ColActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 import com.jru.mycoor.R;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 /**
  * Created by 陈丹丰 on 2016/7/17.
@@ -14,18 +12,12 @@ import butterknife.InjectView;
 public class ColActivity extends Activity {
 
 
-    @InjectView(R.id.toolBar)
-    android.support.v7.widget.Toolbar toolBar;
-    @InjectView(R.id.coll_toolbar)
-    android.support.design.widget.CollapsingToolbarLayout collToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_col);
-        ButterKnife.inject(this);
 
-        collToolbar.setTitle("hello Coll");
 
     }
 }
